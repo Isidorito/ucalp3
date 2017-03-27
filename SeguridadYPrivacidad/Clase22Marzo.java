4cfd560fc03132e5c7c850ebb3ce4cd7dbda8f0f
Isidorito <isidorito@outlook.com>
2017-03-27 19:11:55 -0300

    Create Tree

---
+++ SeguridadYPrivacidad/Clase22Marzo.java
@@ -0,0 +1,72 @@
+/**
+
+	@Clase: Seguridad y Privacidad
+	@Date:  22/03/2017
+	@NoteS: Definiciones NO de memoria PERO si CON lenguaje academico
+
+*/
+
+// Seminario: "Seguridad Informatica"
+	
+	Contenido
+	
+		Conceptos
+		Objetivos
+		Servicios de la Seg Informatica
+		Sistemas de Gestión de Seg Informatica
+	
+	Definiciones:
+		Seguridad: 	Confidencialidad 					//C
+					Integridad 							//I
+					Disponibilidad(Availability)		//A
+				
+			Estos 3 puntos son muy importantes, la preservación de estos es fundamental para la seguridad informatica.
+		
+		-Medidas y controles toados por nosotros para proteger la CIA
+		
+		-Activo de Información: todo recurso brindado o relacionado con el Sistema, necesarios para que la org funcione.
+		
+		-Principio de "Defensa en Profundidad"
+			Seguridad en modo cebolla, son varias barreras que tienen distintos requerimentos para que todo esté asegurado.
+			
+	Objetivos:
+		- Minimizar y gestionar riesgos para detectar posibles amenazas a la seguridad.
+		- Garantizar que los recursos se usen correctamente
+		- Limitar perdidas y maximizar recuperación del sistema
+		- Cumplir con el marco legal y de clientes
+		
+		Planos:
+			-Humano:
+				- Es más barato concientizar/capacitar a los usuarios que arreglar lo que rompen.
+				- Cunciones y obligaciones del personal
+				- Control y supervisión de personal -> Feedback -> Intención de mejora continua
+			
+			-Tecnico:
+				- Configuraciones, sistemas, permisos...
+				- Criptografía
+				- Standarización de productos
+				- Desarrollo seguro de aplicaciones
+			
+			-Organizativo:
+				- Politicas : Ideas (Intenciones de alto nivel) que rigen el rumbo de la organización.
+				- Normas: Como se hacen las ideas
+				- Procedimientos: Como se hacen las ideas
+				- Planes de contingencia
+				- Relacones con stakeholders 
+			
+			-Legal: 	
+				-Cumplimiento y adaptación al marco legal.
+				
+	Servicios:
+		- Confidencialidad: Cada mensaje deben ser leidos por su legitimo destinatario
+		- Autenticación: Garantizar la identidad del generador del mensaje
+		- Integridad: Que los mensajes enviados no sean modificados en el camino entre emisor y receptor
+		- No repudiación: Buscar un mecanismo probatorio y que el usuario no se pueda negar el envío
+		- Disponibilidad: Garantizar que la información esté disponible en el momento y lugar requerido
+		- Autorización: Mecanismo para limitar el acceso a servicios a distintos usuarios
+		- Auditabilidad: Registrar movimientos (logs)
+		- Reclamación de origen: Probar que el autor del mensaje es quién dice ser
+		- Reclamación de propiedad: Ver si un documento tiene derechos de autor 
+		
+		 
+	
---
+++ Teologia2/claseMarzo27.java
@@ -0,0 +1,78 @@
+/**
+@Class: Teología2
+@Date: 27/03/2017
+*/
+
+<link> http://www.vatican.va/archive/catechism_sp/p1s2c1p5_sp.html#I Los ángeles </link>
+El Cielo y la tierra (325-327)
+Los angeles (328)
+Quienes son los angeles (329-330)
+Cristo "Con todos sus Angeles" (331-333)
+Los Angeles en la vida de la Iglesia (334-336)
+
+		Símbolo de los Apóstoles  	-> El "Simbolo" es el 'Credo'  	el corto que es el que usualmente rezamos es el que se le atibuye a los apostoles
+		Símbolo de Nicea											es un desarrollo + largo que se elabora durante el Concilio de Nicea
+		"cielo y tierra" 			-> Esta expresión expresa toda la creación
+									-> Dios crea 2 criaturas -Angelicas -> Naturaleza: 	Ser personal
+																						Puramente espirituales
+																						dotados de 	-Inteligencia
+																									-Voluntad libre
+																		-> Misión:		ALABAR a dios
+																						Llevar a los hombres el mensaje de dios/divino
+																						//están siempre en presencia de dios
+																		-> Funciones:	Interceden por nosotros ante dios
+																						Acompañan a todos y cada uno de los seres humanos
+																						
+																		-> Los angeles caidos: 	Son aquellos que lo niegan o se ponen en su contra
+																								Deja de ser una felicidad contemplar a dios para ellos
+																								Al conocer a Dios tienen solo una oportunidad de aceptarlo
+																								No pierden sus "poderes", pueden comunicar a los mortales, pero comunican lo contrario, la mentira
+																								Jesús los vence cuando viene al mundo
+																								La diferencia con el pecado del hombre es esa unica chance, 
+																									porque lo conoce directamente a dios, 
+																									en cambio el hombre lo va conociendo de a poco
+																									Dios es mas misericordioso con el hombre por ser menos capaz de reconocer a dios 
+																										los angeles envidian a los hombres para eso
+El mundo visible 	(337-341)
+					(342-345)
+					(346-349)
+															-Mundanas (del mundo)	-> El hombre es una mezcla de ambos
+															-Dios ha creado todo
+															-Secuencia de los 6 días			-> el 7 debe ser dedicado a la contemplación de la obra de dios y a alabarlo 	
+																															->	sábat = sabado
+																															-> 	Luego pasa al domingo porque es el "8" día 
+																																	el día después del tiempo 
+																																	es el día en que Cristo resucitó
+															-Todo existe por díos
+															-Criatura: 	perfección y bondad propia
+																		reflejo de	-> 	inteligencia		de dios		-> contemplando esta belleza descubrimo la existencia de dios
+																						harmonía
+																						belleza
+																						poder 
+															-Interdependencia de las criaturas	-> se complementan y sirven mutuamente	->	Existe una solidaridad entre las criaturas pero hay una jerarquía
+															-Armonía del mundo creado
+															-Valor de la persona humana
+															-El sentido de la creación 	-> Todo apunta a la adoración a dios
+El hombre (355)
+a imagen de dios 	(356-357)
+					(358-361)
+															-Unica criatura que			->  Reconoce a dios	
+																							Ama a dios
+															-Creado para servir y amar a dios
+															-Adán 	->	Cabeza de toda la humanidad
+																		Cuando peca, el castigo recae sobre toda la humanidad
+															-Cristo	->	Es el segundo cabeza de la humanidad	
+																		Es la forma en que la humanidad se redime
+																		Por el entra el bien nuevamente en la huanidad
+																							
+																							
+																							
+																							
+																							
+																
+																
+																
+																
+
+
+
