43f32a19067a1efa1a18242082f065ebbd87f778
Isidorito <isidorito@outlook.com>
2017-03-27 20:34:02 -0300

    Carga Materia Organización y Sistemas

---
+++ Organización YSistemas/claseMarzo27.java
@@ -0,0 +1,60 @@
+/**
+@Class: Organización y Sistemas
+@Date: 27/03/2017
+*/
+		
+		_____________________________________
+	   |									 |
+	   |			Sociedad				 |	//Entorno
+	   |_____________________________________|
+	   |									 |
+	   |			Organización		     |	//Puede ser Entorno-> de un Sistema MUY focalizado
+	   |_____________________________________|
+	   |									 |
+	   |		Procesos Empresariales		 |	
+	   |_____________________________________|
+	   |									 |
+	   |			Aplicación 	 			 |	//Software  
+	   |_____________________________________|
+	   |									 |
+	   |			Comunicación 			 |	//Midleware
+	   |_____________________________________|
+	   |									 |
+	   |				SO 					 |	
+	   |_____________________________________|
+	   |									 |
+	   |			  Equipo				 |	//Hardware
+	   |_____________________________________|
+	   
+	Todo esto está relacionado en todos los puntos con las personas 
+		-Usual rechazo a lo nuevo
+		
+	A mayor rango 	->	menor especialización operativa
+					->  mayor responsabilidad 
+	
+	*//si el operario no lo usa, no sirve 	-> 	lo convierte en el mas importante
+	*   	El software no quita el trabajo al operario	->	jerarquiza su trabajo
+	*													->	especializa
+	*													-> 	evita errores
+	*													->	mejora de calidad del producto final
+	*
+	Dato 		-> valor "pelado"
+	Información	-> Dato procesado
+	Un Sistema de la información 	->	es abstracto 	-> 	es necesario plasmarlo
+									-> 	es Socio-Tecnico
+									
+	Categoría de Sistemas de la información	->	de Procesamiento de Transacciones			
+														//Transacción: Conjunto de acciones atomicas que coienzan y terminan, si no se puede terminar, no se ejecuta								
+											->	de Información Administrativa
+														//Facilitan la toma de decisiones en la parte administrativa de una empresa
+											->  de Soporte de decisiones
+														//Proveen información gerencial -> Apunta a directivos	
+	
+	Usuario 	->	Categorías	->	Operacionales		//Según nivel de supervisión
+								->	Supervisores
+								->	Ejecutivos
+							
+
+								
+												
+		
